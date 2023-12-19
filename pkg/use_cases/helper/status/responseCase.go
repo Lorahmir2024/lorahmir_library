f4604b96dc63f17832c32f517258d97bab7b2f73
Giancarlo <gianca9405@gmail.com>
2023-12-19 17:29:51 -0300

    update

--- pkg/use_cases/helper/status/responseCase.go
+++ pkg/use_cases/helper/status/responseCase.go
@@ -21,16 +21,14 @@ func ResponseCase(c echo.Context, status response.Status, data interface{}) erro
 	case response.SearchSuccess, response.CreateSuccess, response.UpdateSuccess, response.DeleteSuccess,
 		response.SearchFailed, response.CreateFailed, response.UpdateFailed, response.DeleteFailed:
 		return c.JSON(http.StatusOK, echo.Map{
-			"status":  status.Index(),
-			"message": status.String(),
-			"data":    respData,
+			"status": status.String(),
+			"data":   respData,
 		})
 
 	case response.InternalServerError, response.DBQueryError, response.DBRowsAffectedError, response.DBExecutionError, response.DBRowsError, response.DBScanError, response.DBInitError:
 		return c.JSON(http.StatusInternalServerError, echo.Map{
-			"status":  status.Index(),
-			"message": status.String(),
-			"data":    respData,
+			"status": status.String(),
+			"data":   respData,
 		})
 
 	case response.NoContent:
@@ -38,45 +36,39 @@ func ResponseCase(c echo.Context, status response.Status, data interface{}) erro
 
 	case response.BadRequest:
 		return c.JSON(http.StatusBadRequest, echo.Map{
-			"status":  status.Index(),
-			"message": status.String(),
-			"data":    respData,
+			"status": status.String(),
+			"data":   respData,
 		})
 
 	case response.Unauthorized:
 		return c.JSON(http.StatusUnauthorized, echo.Map{
-			"status":  status.Index(),
-			"message": status.String(),
-			"data":    respData,
+			"status": status.String(),
+			"data":   respData,
 		})
 
 	case response.Conflict, response.FormatInvalid:
 		return c.JSON(http.StatusConflict, echo.Map{
-			"status":  status.Index(),
-			"message": status.String(),
-			"data":    respData,
+			"status": status.String(),
+			"data":   respData,
 		})
 
 	case response.RequestEntityTooLarge:
 		return c.JSON(http.StatusRequestEntityTooLarge, echo.Map{
-			"status":  status.Index(),
-			"message": status.String(),
-			"data":    respData,
+			"status": status.String(),
+			"data":   respData,
 		})
 
 	case response.UnsupportedMediaType:
 		return c.JSON(http.StatusUnsupportedMediaType, echo.Map{
-			"status":  status.Index(),
-			"message": status.String(),
-			"data":    respData,
+			"status": status.String(),
+			"data":   respData,
 		})
 
 	default:
 		status = response.Unknown
 		return c.JSON(http.StatusNotImplemented, echo.Map{
-			"status":  status.Index(),
-			"message": status.String(),
-			"data":    respData,
+			"status": status.String(),
+			"data":   respData,
 		})
 	}
 }
