417bb5907bbcc8197ccdf306088de735c0964939
Gianca <44784488+gianca1994@users.noreply.github.com>
2024-10-09 19:11:18 -0300

    Update status.go

--- pkg/domain/response/status.go
+++ pkg/domain/response/status.go
@@ -7,10 +7,66 @@ const (
 	InternalServerError Status = iota
 	OK
 	Unknown
+	BadRequest
+	Unauthorized
+	Forbidden
+	MethodNotAllowed
+	Conflict
+	ServiceUnavailable
+	Timeout
+	UnsupportedMediaType
+	TooManyRequests
+	NotAcceptable
+	PreconditionFailed
+	Gone
+	UnprocessableEntity
+	RequestTimeout
+	LengthRequired
+	ExpectationFailed
+	PayloadTooLarge
+	UriTooLong
+	NotImplemented
+	BadGateway
+	ServiceTimeout
+	InsufficientStorage
+	LoopDetected
+	NetworkAuthenticationRequired
+	UnavailableForLegalReasons
+	ProxyAuthenticationRequired
+	FailedDependency
+	NotFound
 	SearchFailed
 	CreateFailed
-	DeleteFailed
 	UpdateFailed
+	DeleteFailed
+	InvalidRequest
+	ResourceAlreadyExists
+	ResourceLocked
+	FetchFailed
+	InvalidParameter
+	DatabaseError
+	ValidationFailed
+	PermissionDenied
+	UnexpectedError
+	TimeoutError
+	MethodNotSupported
+	PatchFailed
+	FetchNotModified
+	UpdateConflict
+	DeleteConflict
+	ExpiredSession
+	InvalidCredentials
+	ResourceNotModified
+	LockedResource
+	UnauthorizedAccess
+	InsufficientPermissions
+	UrlNotFound
+	ForbiddenAction
+	IncompleteData
+	SessionTimeout
+	ApiVersionNotSupported
+	InactiveAccount
+	BlockedAccount
 	DBInitError
 	DBQueryError
 	DBExecutionError
@@ -18,41 +74,107 @@ const (
 	DBRowsError
 	DBRowsAffectedError
 	NoContent
-	BadRequest
-	Conflict
 	FormatInvalid
 	DecodeError
-	Unauthorized
 	RequestEntityTooLarge
-	UnsupportedMediaType
-	UsernamesDoNotMatch
+	ServiceTemporarilyUnavailable
+	PreconditionRequired
+	TooManyConnections
+	InvalidApiKey
+	InactiveSession
+	ResourceNotAvailable
+	UnsupportedVersion
+	FeatureNotAvailable
+	RequestRateLimited
+	OperationNotAllowed
 )
 
 // LOS STATUS DEBEN MANTENER SU ORDEN. LOS NUEVOS DEBEN SER AGREGADOS AL FINAL
 func (s Status) String() string {
 	return [...]string{
-		"InternalServerError",
-		"OK",
-		"Unknown",
-		"SearchFailed",
-		"CreateFailed",
-		"DeleteFailed",
-		"UpdateFailed",
-		"DBInitError",
-		"DBQueryError",
-		"DBExecutionError",
-		"DBScanError",
-		"DBRowsError",
-		"DBRowsAffectedError",
-		"NoContent",
-		"BadRequest",
-		"Conflict",
-		"FormatInvalid",
-		"DecodeError",
-		"Unauthorized",
-		"RequestEntityTooLarge",
-		"UnsupportedMediaType",
-		"UsernamesDoNotMatch",
+		 "InternalServerError",
+		 "OK",
+		 "Unknown",
+		 "BadRequest",
+		 "Unauthorized",
+		 "Forbidden",
+		 "MethodNotAllowed",
+		 "Conflict",
+		 "ServiceUnavailable",
+		 "Timeout",
+		 "UnsupportedMediaType",
+		 "TooManyRequests",
+		 "NotAcceptable",
+		 "PreconditionFailed",
+		 "Gone",
+		 "UnprocessableEntity",
+		 "RequestTimeout",
+		 "LengthRequired",
+		 "ExpectationFailed",
+		 "PayloadTooLarge",
+		 "UriTooLong",
+		 "NotImplemented",
+		 "BadGateway",
+		 "ServiceTimeout",
+		 "InsufficientStorage",
+		 "LoopDetected",
+		 "NetworkAuthenticationRequired",
+		 "UnavailableForLegalReasons",
+		 "ProxyAuthenticationRequired",
+		 "FailedDependency",
+		 "NotFound",
+		 "SearchFailed",
+		 "CreateFailed",
+		 "UpdateFailed",
+		 "DeleteFailed",
+		 "InvalidRequest",
+		 "ResourceAlreadyExists",
+		 "ResourceLocked",
+		 "FetchFailed",
+		 "InvalidParameter",
+		 "DatabaseError",
+		 "ValidationFailed",
+		 "PermissionDenied",
+		 "UnexpectedError",
+		 "TimeoutError",
+		 "MethodNotSupported",
+		 "PatchFailed",
+		 "FetchNotModified",
+		 "UpdateConflict",
+		 "DeleteConflict",
+		 "ExpiredSession",
+		 "InvalidCredentials",
+		 "ResourceNotModified",
+		 "LockedResource",
+		 "UnauthorizedAccess",
+		 "InsufficientPermissions",
+		 "UrlNotFound",
+		 "ForbiddenAction",
+		 "IncompleteData",
+		 "SessionTimeout",
+		 "ApiVersionNotSupported",
+		 "InactiveAccount",
+		 "BlockedAccount",
+		 "DBInitError",
+		 "DBQueryError",
+		 "DBExecutionError",
+		 "DBScanError",
+		 "DBRowsError",
+		 "DBRowsAffectedError",
+		 "NoContent",
+		 "FormatInvalid",
+		 "DecodeError",
+		 "RequestEntityTooLarge",
+		 "ServiceTemporarilyUnavailable",
+		 "PreconditionRequired",
+		 "TooManyConnections",
+		 "InvalidApiKey",
+		 "InactiveSession",
+		 "ResourceNotAvailable",
+		 "UnsupportedVersion",
+		 "FeatureNotAvailable",
+		 "RequestRateLimited",
+		 "OperationNotAllowed",
 	}[s]
 }
 
